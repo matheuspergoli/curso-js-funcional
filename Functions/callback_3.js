fa496d580084361551c6e00bde7351ccbca0446f
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-29 03:13:35 -0300

    callback3

--- Functions/callback_3.js
+++ Functions/callback_3.js
@@ -2,13 +2,13 @@ const numeros = [1, 2, 3, 4, 5]
 
 const dobro = n => n * 2
 
-console.log(numeros.map(dobro))
+// console.log(numeros.map(dobro))
 
-const nomes = ['Ana', 'Bia', 'Gui', 'Lia', 'Rafa']
+// const nomes = ['Ana', 'Bia', 'Gui', 'Lia', 'Rafa']
 
-const primeiraLetra = texto => texto[0]
-const letras = nomes.map(primeiraLetra)
-console.log(nomes, letras)
+// const primeiraLetra = texto => texto[0]
+// const letras = nomes.map(primeiraLetra)
+// console.log(nomes, letras)
 
 const carrinho = [
     { nome: 'Caneta', qtde: 10, preco: 7.99 },
@@ -17,3 +17,9 @@ const carrinho = [
     { nome: 'Lapis', qtde: 3, preco: 5.82 },
     { nome: 'Tesoura', qtde: 1, preco: 19.20 }
 ]
+
+const getNome = item => item.nome
+console.log(carrinho.map(getNome))
+
+const getTotal = item => item.qtde * item.preco 
+console.log(carrinho.map(getTotal))
